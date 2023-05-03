6a0c096c499f92e4af21501e3c229ae2ec72fad4
yudaeon <daeon03050@gmail.com>
2023-05-03 21:56:40 +0900

    feat: java-project-study_Section6

---
+++ src/main/java/chap_06/_04_ParameaterAndReturn.java
@@ -0,0 +1,31 @@
+package chap_06;
+
+public class _04_ParameaterAndReturn {
+
+    public static int getpower(int number) { //전달값
+        int result = number * number;
+        return result;
+    }
+
+    public static int getPowerByExp(int number, int exponent) {
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= number;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) { //반환값
+        //전달값과 반환값이 함께있는 메소드 만들기
+       int retVal =  getpower(2); //2*2=4 호출
+        System.out.println(retVal);
+
+        retVal = getpower(3);
+        System.out.println(retVal);
+
+        retVal = getPowerByExp(3, 3);
+        System.out.println(retVal); //3*3*3 = 27s
+
+        System.out.println(getPowerByExp(2,4));
+    }
+}
---
+++ src/main/java/chap_06/_07_VariableScope.java
@@ -0,0 +1,28 @@
+package chap_06;
+
+public class _07_VariableScope {
+    public static void methodA(int number){
+        //System.out.println(number);
+        //System.out.println(result);
+    }
+    public static void methodB(){
+        int result = 1; //지역변수 ( 변수가 선언된 영역내에서만 사용가능)
+    }
+
+    public static void main(String[] args) {
+        int number = 3;
+
+        // System.out.println(result);
+
+        for (int i = 0; i <5 ; i++) {
+            System.out.println(i);
+        }
+      //  System.out.println(i);
+        {
+            int j = 0;
+            System.out.println(j);
+            System.out.println(number); //괄호안에서면 사용 가능
+        }
+      //  System.out.println(j);
+        }
+    }
