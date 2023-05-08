d73c7beb0b5311ca51eadc425a9979b2c38e3e3d
yudaeon <daeon03050@gmail.com>
2023-05-08 17:40:46 +0900

    feat: sectio8 클래스 - 생성자

--- src/main/java/chap_07/BlackBox.java
+++ src/main/java/chap_07/BlackBox.java
@@ -6,9 +6,30 @@ public class BlackBox {
     String resolution; //해상도
     int price; // 가격
     String color; //색상
+    int serialNumber; //시리얼 번호
 
+    static int counter = 0; //시리얼 번호를 생성해주는 역할( 처음엔 0이었다가 ++ 연산을 통해서 값을 증가)
     //static~ 클래스 내에 클래스 변수
     static boolean canAutoReport = false; //자동신고기능
+
+    //생성자 만들기(클래스명과 동일하게)
+    BlackBox(){
+
+        System.out.println("기본 생성자 호출");
+        this.serialNumber = ++counter;
+        System.out.println("새로운 시리얼 넘버를 발급받았습니다 : " + this.serialNumber);
+    }
+
+    BlackBox(String modelName, String resolution,int price, String color){
+        this(); //기본생성자호출
+
+        System.out.println("사용자 정의 생성자 호출");
+        this.modelName = modelName;
+        this.resolution = resolution;
+        this.price = price;
+        this.color = color;
+    }
+
 
     void autoReport() {
      if(canAutoReport) {
